bfbe093d8b12463a2fc72f7da0c1411676014251
Alejandro Di Miscia <alejandro.di@enti.cat>
2024-11-14 10:50:10 +0100

    Check FILENAME configurado

--- client.sh
+++ client.sh
@@ -6,12 +6,20 @@ echo "Cliente de Dragón Magia Abuelita Miedo 2022"
 
 echo "1. ENVÍO DE CABECERA"
 
-echo "DMAM202" | nc localhost $PORT
+echo "DMAM2022" | nc localhost $PORT
 
 DATA=`nc -l $PORT`
+
+echo "3. COMPROBACION DE CABECERA"
 
 if [ "$DATA" != "OK_HEADER" ]
 then
 	echo "ERROR 1: La Cabecera Se Envió Incorrectamente"
 	exit 1
 fi
+
+echo "4. ENVIO NOMBRE DE ARCHIVO"
+
+echo "FILE_NAME dragon.txt" | nc localhost $PORT
+
+nc -l $PORT
--- server.sh
+++ server.sh
@@ -17,3 +17,19 @@ fi
 
 echo "2. CHECK OK - Enviando OK_HEADER"
 echo "OK_HEADER" | nc localhost 2022
+
+echo "5. CHECK FILENAME"
+FILENAME=`nc -l $PORT`
+
+PREFIJO=`echo "$FILENAME" | cut -d' ' -f1`
+
+if [ "$PREFIJO" != "FILE_NAME" ]
+then
+ 	echo "ERROR 1: Prefijo Incorrecto"
+ 	echo "KO_FILENAME" | nc localhost 2022
+ 	exit 1
+fi
+
+echo "6. CHECK OK - Enviando OK_FILENAME"
+echo "Enviando OK_FILENAME..."
+echo "OK_FILENAME" | nc localhost 2022
